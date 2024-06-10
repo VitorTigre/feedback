187a280bc70aaf5e869b81f4ad83512f5c5b1d90
VitorTigre Tigre <vitorrtigre@gmail.com>
2024-06-10 12:44:47 +0000

    descobri que esqueci de dar git push em um commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,58 @@
-import { ButtonContainer, CardContainer, IconContainer, ResultContainer} from "./Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
 
-import iconStarImg from "../assets/icon-star.svg";
-import thankYouImg from "../assets/ilustration-thank-you.svg"
-
+import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
+import { useState } from "react";
 
 export function Home() {
-    let aparecerResultado = true
+  const [mostrarResultado, setMostrarResultado] = useState(false)
+  const [feedbackNote, setFeedbackNote] = useState(0)
 
-    return(
-        aparecerResultado === false ? (
-            <CardContainer>
-                <IconContainer>
-                    <img src="{iconStarImg}" alt="ícone de estrela" />
-                </IconContainer>
-    
-                <h1>Como foi o atendimento?</h1>
-                <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento</p>
-    
-                <ButtonContainer>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                </ButtonContainer>
-    
-                <button>Enviar</button>
-            </CardContainer>
-        ) : (
-            <CardContainer>
-                <img src={thankYouImg} alt="Imagem de agradecimento" />
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText)
 
-                <ResultContainer>
-                    <p>Você selecionou 4 de 5</p>
-                </ResultContainer>
+    setFeedbackNote(feedback)
+  }
 
-                <h1>Obrigado!</h1>
+  function handleSubmit() {
+    if (feedbackNote === 0) return
 
-                <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
-            </CardContainer>
-        )
-    )
+    setMostrarResultado(true)
+  }
+
+  return (
+    mostrarResultado === false ? (
+      <CardContainer>
+        <IconContainer >
+          <img src={iconStarImg} alt="estrela" />
+        </IconContainer>
+
+
+        <h1>Como foi o atendimento?</h1>
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+        <ButtonContainer >
+          <button onClick={handleFeedbackButtonClick}>1</button>
+          <button onClick={handleFeedbackButtonClick}>2</button>
+          <button onClick={handleFeedbackButtonClick}>3</button>
+          <button onClick={handleFeedbackButtonClick}>4</button>
+          <button onClick={handleFeedbackButtonClick}>5</button>
+        </ButtonContainer>
+
+        <button onClick={handleSubmit}>Enviar</button>
+    </CardContainer>
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="iamgem de agradecimento" />
+
+        <ResultContainer>
+          <p>Você selecionou {feedbackNote} de 5</p>
+        </ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+    </CardContainer>
+    )
+  )
 }
--- src/pages/Home.styles.js
+++ src/pages/Home.styles.js
@@ -3,79 +3,77 @@ import styled from "styled-components"
 export const CardContainer = styled.div`
     background: ${props => props.theme.cardGradient};
     max-width: 41.2rem;
-    padding:3.2rem;
+    padding: 3.2rem;
     border-radius: 3rem;
-    
 
-        h1 {
-            font-size: 2.8rem;
-            color: ${props => props.theme.white};
-            margin-bottom: 1.5rem;
-            text-align: center;
-        }
+    h1 {
+        font-size: 2.8rem;
+        color: ${props => props.theme.white};
+        margin-bottom: 1.5rem;
+        text-align: center;
+    }
 
-        p {
-            font-size: 1.5rem;
-            line-height: 2.4rem;
-            color: ${props => props.theme.textColor};
-            text-align: center;
-        }
+    p {
+        font-size: 1.5rem;
+        line-height: 2.4rem;
+        color: ${props => props.theme.textColor};
+        text-align: center;
+    }
 
-        & > button {
-            width:100%;
-            padding-block: 1.1rem;
-            border-radius: 2.25rem;
-            border:none;
-            font-size: 1.5rem;
-            font-weight: 700;
-            letter-spacing: 2px;
-            text-transform: uppercase;
-            background:  ${props => props.theme.submitButtonColor};
-            color:  ${props => props.theme.white};
-            cursor: pointer;
-            transition: all .3s;
+    & > button {
+        width: 100%;
+        padding-block: 1.1rem;
+        border-radius: 2.25rem;
+        border: none;
+        font-size: 1.5rem;
+        font-weight: 700;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+        background: ${props => props.theme.submitButtonColor};
+        color: ${props => props.theme.white};
+        cursor: pointer;
+        transition: all .3s;
 
         &:hover {
-            background:  ${props => props.theme.white};
-            color:  ${props => props.theme.submitButtonColor};
+            background: ${props => props.theme.white};
+            color: ${props => props.theme.submitButtonColor};
         }
     }
- 
+
     & > img {
         display: block;
         margin: 1.3rem auto 3.2rem;
-
     }
 `
 
+
 export const IconContainer = styled.div`
-        background: ${props => props.theme.feedbackButtonColor};
-        width: 4.8rem;
-        height:4.8rem;
-        padding: 1.6rem;
-        border-radius: 50%;
+    background: ${props => props.theme.feedbackButtonColor};
+    width: 4.8rem;
+    height: 4.8rem;
+    border-radius: 50%;
 
-        display:flex;
-        justify-content: center;
-        align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-        margin-bottom: 3rem;
-    `
+    margin-bottom: 3rem;
 
+`
 
 export const ButtonContainer = styled.div`
     margin-top: 2.4rem;
     margin-bottom: 3.2rem;
-    display:flex;
+    display: flex;
     justify-content: space-between;
 
-    button{
+    button {
         width: 5.1rem;
         height: 5.1rem;
         border-radius: 50%;
         border: none;
-        background:  ${props => props.theme.feedbackButtonColor};
-        color:  ${props => props.theme.feedbackButtonText};
+        background: ${props => props.theme.feedbackButtonColor};
+        color: ${props => props.theme.feedbackButtonText};
         font-weight: 700;
         line-height: 2.4rem;
         cursor: pointer;
@@ -83,25 +81,26 @@ export const ButtonContainer = styled.div`
 
         &:focus {
             color: ${props => props.theme.white};
-            background:  ${props => props.theme.feedbackButtonText};
+            background: ${props => props.theme.feedbackButtonText};
         }
 
         &:hover {
             color: ${props => props.theme.white};
-            background;
+            background: ${props => props.theme.submitButtonColor};
         }
+
     }
 `
 
 export const ResultContainer = styled.p`
     padding: 0.4rem 1.8rem;
-    background:${props => props.theme.feedbackButtonColor};;
+    background: ${props => props.theme.feedbackButtonColor};
     width: fit-content;
     margin-inline: auto;
     margin-bottom: 3.2rem;
-    border-radius: 2.25rem
-
+    border-radius: 2.25rem;
+    
     p {
         color: ${props => props.theme.submitButtonColor};
-    }
+    }
 `
