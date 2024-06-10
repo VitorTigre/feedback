34cff1018503340117b2a90340598fc674a7e639
VitorTigre Tigre <vitorrtigre@gmail.com>
2024-06-10 03:40:44 +0000

    stye: criação dos estilos globais

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { redirect } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+
+        padding: 0;
+
+        box-sizing: border-box;
+    }
+`
